5f76497375560a87c989f94433eeff14d53ad422
isard <isard@debian>
2026-01-14 14:12:55 +0100

    nova app

---
+++ polls/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    #@admin.display(
+        #boolean=True,
+        #ordering="pub_date",
+        #description="Published recently?",
+    #)
+    #def was_published_recently(self):
+        #now = timezone.now()
+        #return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
---
+++ polls/testbackup.py
@@ -0,0 +1,50 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth.models import User   # ← AÑADIDO
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+ 
+class MySeleniumTests(StaticLiveServerTestCase):
+    # carregar una BD de test
+    #fixtures = ['testdb.json',]
+ 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        User.objects.create_superuser(
+            username='isard',
+            email='isard@test.com',
+            password='pirineus'
+        )
+
+        opts = Options()
+        cls.selenium = WebDriver(options=opts)
+        cls.selenium.implicitly_wait(5)
+ 
+    @classmethod
+    def tearDownClass(cls):
+        # tanquem browser
+        # comentar la propera línia si volem veure el resultat de l'execució del navegador
+        cls.selenium.quit()
+        super().tearDownClass()
+ 
+    def test_login(self):
+        # anem directament a la pàgina d'accés a l'admin panel
+        self.selenium.get('%s%s' % (self.live_server_url, '/admin/login/'))
+ 
+        # comprovem que el títol de la pàgina és el que esperem
+        self.assertEqual(self.selenium.title, "Log in | Django site admin")
+ 
+        # introduïm dades de login i cliquem el botó "Log in" per entrar
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys('isard')
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys('pirineus')
+        self.selenium.find_element(By.XPATH, '//input[@value="Log in"]').click()
+ 
+        # comprovem si hem aconseguit entrar a l'admin panel pel títol de la pàgina
+        self.assertEqual(
+            self.selenium.title,
+            "Site administration | Django site admin"
+        )
